53c8817005aeefb7bd62836f6119148a9ed3fb2d
Gabriel Banfalvi <gabriel.banfalvi@klarna.com>
2019-07-31 11:54:48 +0200

    [Lib] Begin adding callbacks.

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ import { requireNativeComponent, UIManager, findNodeHandle } from 'react-native'
 import PropTypes from 'prop-types';
 
 class KlarnaReactPaymentView extends React.Component {
+
     render() {
         return <KlarnaPaymentView {...this.props} />
     }
@@ -15,26 +16,47 @@ class KlarnaReactPaymentView extends React.Component {
         )
     }
 
-    load = () => {
+    load = (sessionData) => {
         UIManager.dispatchViewManagerCommand(
             findNodeHandle(this),
             UIManager.getViewManagerConfig('KlarnaPaymentView').Commands.load,
-            []
+            [sessionData || null]
         )
     }
 
-    authorize = (autoFinalize) => {
+    authorize = (autoFinalize, sessionData) => {
         UIManager.dispatchViewManagerCommand(
             findNodeHandle(this),
             UIManager.getViewManagerConfig('KlarnaPaymentView').Commands.authorize,
-            [autoFinalize || true]
+            [autoFinalize || true, sessionData || null]
+        )
+    }
+
+    reauthorize = (autoFinalize, sessionData) => {
+        UIManager.dispatchViewManagerCommand(
+            findNodeHandle(this),
+            UIManager.getViewManagerConfig('KlarnaPaymentView').Commands.reauthorize,
+            [autoFinalize || true, sessionData || null]
+        )
+    }
+
+    finalize = (autoFinalize, sessionData) => {
+        UIManager.dispatchViewManagerCommand(
+            findNodeHandle(this),
+            UIManager.getViewManagerConfig('KlarnaPaymentView').Commands.finalize,
+            [autoFinalize || true, sessionData || null]
         )
     }
 }
 
 KlarnaReactPaymentView.propTypes = {
     category: PropTypes.string,
-    onEvent: PropTypes.func
+    onInitialized: PropTypes.func,
+    onLoaded: PropTypes.func,
+    onAuthorized: PropTypes.func,
+    onReauthorized: PropTypes.func,
+    onFinalized: PropTypes.func,
+    onError: PropTypes.func,
 }
 
 const KlarnaPaymentView = requireNativeComponent('KlarnaPaymentView', KlarnaReactPaymentView);
