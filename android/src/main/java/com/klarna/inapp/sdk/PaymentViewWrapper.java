ad2a6c4726f7c71f56bbaee19c4480ae0749f571
Mahmoud <mahmoud.jafarinejad@klarna.com>
2020-06-23 13:56:44 +0200

    added resize observer to payment container in android to update the view's height wrapped the resize observer js script with try catch + reformat

--- android/src/main/java/com/klarna/inapp/sdk/PaymentViewWrapper.java
+++ android/src/main/java/com/klarna/inapp/sdk/PaymentViewWrapper.java
@@ -1,8 +1,9 @@
 package com.klarna.inapp.sdk;
 
+import android.os.Build;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
 import android.webkit.ValueCallback;
 import android.webkit.WebView;
 import android.widget.LinearLayout;
@@ -19,6 +20,7 @@ public class PaymentViewWrapper extends LinearLayout {
 
     private float displayDensity = 1;
     public KlarnaPaymentView paymentView;
+    private boolean attachedResizeObserver;
 
     public PaymentViewWrapper(ReactApplicationContext context, AttributeSet attrs) {
         super(context, attrs);
@@ -30,36 +32,39 @@ public class PaymentViewWrapper extends LinearLayout {
         LinearLayout.LayoutParams webViewParams = new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
         paymentView = new KlarnaPaymentView(getReactAppContext().getCurrentActivity(), attrs); // Insure we use activity and not application context for dialogs.
         addView(paymentView, webViewParams);
+
+        getPaymentViewWebView().addJavascriptInterface(this, "AndroidResizeObserver");
     }
 
     /**
      * The web view inside the KlarnaPaymentView will trigger requestLayout(), but there is no way
      * to get the KlarnaPaymentView's correct height.
-     *
+     * <p>
      * Attempts include:
      * - Using Android's measure() and co. methods
      * - Getting the WebView's getContentHeight()
      * - Using a variety of layout listners.
-     *
+     * <p>
      * So instead we're:
      * 1. Evaluating some JS (yes, making things even more fragile).
      * 2. Triggering a size change via the UIManagerModule. This will effectively apply a fixed size
-     *    via width and height style attributes on the React side of things.
-     *
+     * via width and height style attributes on the React side of things.
+     * <p>
      * Note: We can apply style by setting props with uimm.updateView() to just set a height (and
-     *       no width), but it doesn't update immediately or with the right height.
-     *
+     * no width), but it doesn't update immediately or with the right height.
+     * <p>
      * The width will be whatever the parent component's width is.
      */
     @Override
     public void requestLayout() {
         super.requestLayout();
-
-        if (!isReady()) {
-            return;
+        if (isReady()) {
+            updateHeight();
         }
+    }
 
-        WebView innerWebView = getPaymentViewWebView();
+    private void updateHeight() {
+        final WebView innerWebView = getPaymentViewWebView();
 
         if (innerWebView == null) {
             return;
@@ -72,12 +77,15 @@ public class PaymentViewWrapper extends LinearLayout {
         innerWebView.evaluateJavascript("document.body.scrollHeight", new ValueCallback<String>() {
             @Override
             public void onReceiveValue(String value) {
-                if(value == null || value.equals("null") || value.equals("undefined")){
+                if (value == null || value.equals("null") || value.equals("undefined")) {
                     return;
                 }
                 try {
                     final float contentHeight = Float.parseFloat(value);
                     final float scaledHeight = contentHeight * displayDensity;
+                    if (scaledHeight > 0 && !attachedResizeObserver) {
+                        attachResizeObserver(innerWebView);
+                    }
                     getReactAppContext().runOnNativeModulesQueueThread(new GuardedRunnable(getReactAppContext()) {
                         @Override
                         public void runGuarded() {
@@ -85,7 +93,7 @@ public class PaymentViewWrapper extends LinearLayout {
                             uimm.updateNodeSize(getId(), viewWidth, (int) scaledHeight);
                         }
                     });
-                } catch (Throwable t){
+                } catch (Throwable t) {
                     return;
                 }
             }
@@ -95,13 +103,16 @@ public class PaymentViewWrapper extends LinearLayout {
     /**
      * Returns true if the payment view is part of the hierarchy (the first requestLayout() is
      * triggered before) and the whole view is part of the RN hierarchy.
+     *
      * @return true if ready
      */
     private boolean isReady() {
         return paymentView != null && getId() != -1;
     }
 
-    /** Returns the parent view's width. */
+    /**
+     * Returns the parent view's width.
+     */
     private int getParentViewWidth() {
         View parentReactView = (View) getParent();
         if (parentReactView == null || !(parentReactView instanceof View)) {
@@ -110,7 +121,9 @@ public class PaymentViewWrapper extends LinearLayout {
         return parentReactView.getWidth();
     }
 
-    /** Attempts to retrieve the web view from within the KlarnaPaymentView. This is really fragile. */
+    /**
+     * Attempts to retrieve the web view from within the KlarnaPaymentView. This is really fragile.
+     */
     private WebView getPaymentViewWebView() {
         for (int i = 0; i < paymentView.getChildCount(); i++) {
             View child = paymentView.getChildAt(i);
@@ -122,8 +135,35 @@ public class PaymentViewWrapper extends LinearLayout {
         return null;
     }
 
-    /** Returns the app context the wrapper was initialized with. */
+    /**
+     * Returns the app context the wrapper was initialized with.
+     */
     private ReactApplicationContext getReactAppContext() {
         return (ReactApplicationContext) getContext();
+    }
+
+    private void attachResizeObserver(WebView webView) {
+        String script =
+                "try{" +
+                        "window.androidResizeObserver = new ResizeObserver(entries => {AndroidResizeObserver.resized();});" +
+                        "window.androidResizeObserver.observe(document.getElementById('payment-container'));" +
+                        "}catch(error){" +
+                        "console.log('Failed to attach AndroidResizeObserver: ' + error)}";
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.evaluateJavascript(script, null);
+        } else {
+            webView.loadUrl("javascript:" + script);
+        }
+    }
+
+    @JavascriptInterface
+    public void resized() {
+        post(new Runnable() {
+            @Override
+            public void run() {
+                attachedResizeObserver = true;
+                updateHeight();
+            }
+        });
     }
 }
