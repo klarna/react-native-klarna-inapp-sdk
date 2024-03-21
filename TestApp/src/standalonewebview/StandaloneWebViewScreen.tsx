9526c8afb32c2ec2b03eb5273a9579fc0201c45a
Masoud Fallahpourbaee <m.fallahpourbaee@klarna.com>
2024-03-21 14:41:55 +0100

    Add a text view behind the StandaloneWebView and make StandaloneWebView's background transparent

--- TestApp/src/standalonewebview/StandaloneWebViewScreen.tsx
+++ TestApp/src/standalonewebview/StandaloneWebViewScreen.tsx
@@ -1,10 +1,10 @@
-import {Keyboard, TextInput, View} from 'react-native';
+import {Keyboard, Text, TextInput, View} from 'react-native';
 import styles from '../common/ui/Styles';
 import React, {useRef, useState} from 'react';
 import {
   KlarnaStandaloneWebView,
-  KlarnaWebViewKlarnaMessageEvent,
   KlarnaWebViewError,
+  KlarnaWebViewKlarnaMessageEvent,
   KlarnaWebViewNavigationEvent,
   KlarnaWebViewProgressEvent,
   KlarnaWebViewRenderProcessGoneEvent,
@@ -112,32 +112,51 @@ export default function StandaloneWebViewScreen() {
         {renderGoBackButton()}
         {renderGoForwardButton()}
       </View>
-      <KlarnaStandaloneWebView
-        ref={klarnaStandaloneWebViewRef}
-        /* eslint-disable-next-line react-native/no-inline-styles */
-        style={{
-          flex: 1,
-        }}
-        returnUrl={'returnUrl://'}
-        onLoadStart={(event: KlarnaWebViewNavigationEvent) => {
-          onEvent('onLoadStart', JSON.stringify(event));
-        }}
-        onLoadEnd={(event: KlarnaWebViewNavigationEvent) => {
-          onEvent('onLoadEnd', JSON.stringify(event));
-        }}
-        onError={(event: KlarnaWebViewError) => {
-          onEvent('onError', JSON.stringify(event));
-        }}
-        onLoadProgress={(event: KlarnaWebViewProgressEvent) => {
-          onEvent('onLoadProgress', JSON.stringify(event));
-        }}
-        onKlarnaMessage={(event: KlarnaWebViewKlarnaMessageEvent) => {
-          onEvent('onKlarnaMessage', JSON.stringify(event));
-        }}
-        onRenderProcessGone={(event: KlarnaWebViewRenderProcessGoneEvent) => {
-          onEvent('onRenderProcessGone', JSON.stringify(event));
-        }}
-      />
+      <View style={{flex: 1}}>
+        <Text
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            textAlign: 'center',
+            backgroundColor: 'lightblue',
+            color: 'black',
+            fontSize: 20,
+            textAlignVertical: 'center',
+          }}>
+          On top of this blue area there is a StandaloneWebView with transparent
+          background!
+        </Text>
+        <KlarnaStandaloneWebView
+          ref={klarnaStandaloneWebViewRef}
+          /* eslint-disable-next-line react-native/no-inline-styles */
+          style={{
+            position: 'absolute',
+            width: '100%',
+            height: '100%',
+            backgroundColor: 'transparent',
+          }}
+          returnUrl={'returnUrl://'}
+          onLoadStart={(event: KlarnaWebViewNavigationEvent) => {
+            onEvent('onLoadStart', JSON.stringify(event));
+          }}
+          onLoadEnd={(event: KlarnaWebViewNavigationEvent) => {
+            onEvent('onLoadEnd', JSON.stringify(event));
+          }}
+          onError={(event: KlarnaWebViewError) => {
+            onEvent('onError', JSON.stringify(event));
+          }}
+          onLoadProgress={(event: KlarnaWebViewProgressEvent) => {
+            onEvent('onLoadProgress', JSON.stringify(event));
+          }}
+          onKlarnaMessage={(event: KlarnaWebViewKlarnaMessageEvent) => {
+            onEvent('onKlarnaMessage', JSON.stringify(event));
+          }}
+          onRenderProcessGone={(event: KlarnaWebViewRenderProcessGoneEvent) => {
+            onEvent('onRenderProcessGone', JSON.stringify(event));
+          }}
+        />
+      </View>
     </View>
   );
 }
