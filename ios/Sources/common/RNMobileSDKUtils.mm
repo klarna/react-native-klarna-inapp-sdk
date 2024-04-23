7f8542ae3a40836dcd67e33400ed26a1b3560ea4
Masoud Fallahpourbaee <m.fallahpourbaee@klarna.com>
2024-04-23 12:39:07 +0200

    Return an empty JSON string when dictionary is nil in serializeDictionaryToJsonString()

--- ios/Sources/common/RNMobileSDKUtils.mm
+++ ios/Sources/common/RNMobileSDKUtils.mm
@@ -3,15 +3,20 @@
 @implementation SerializationUtil
 
 + (id)serializeDictionaryToJsonString:(id)dictionary {
+    if (!dictionary) {
+        NSLog(@"Dictionary is nil");
+        return @"{}";
+    }
+     
     NSError *error;
-        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject:dictionary options:NSJSONWritingPrettyPrinted error:&error];
 
-        if (!jsonData) {
-            return @"{}";
-        } else {
-            NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
-            return jsonString;
-        }
+    if (!jsonData) {
+        return @"{}";
+    } else {
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        return jsonString;
+    }
 }
 
 @end
