4510432907e7e222007a97e363ada110a68b3538
Nuri Mertcan Guner <NMGuner@users.noreply.github.com>
2024-05-17 00:50:05 +0200

    update dependencies and minor issues

--- TestApp/android/app/src/main/java/com/klarna/mobile/sdk/reactnative/testapp/MainApplication.kt
+++ TestApp/android/app/src/main/java/com/klarna/mobile/sdk/reactnative/testapp/MainApplication.kt
@@ -3,8 +3,11 @@ package com.klarna.mobile.sdk.reactnative.testapp
 import android.app.Application
 import com.facebook.react.PackageList
 import com.facebook.react.ReactApplication
+import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
+import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
 
