32de71e290313dac3e0547d5daf47d3f8ef1b24b
Jorge Palacio <jorge.palacio@klarna.com>
2025-02-25 14:10:43 +0100

    MSDK-892 Added module name to match turbo generated one, also implemented new protocols in whe manager class

--- ios/Sources/signIn/KlarnaSignInManager.mm
+++ ios/Sources/signIn/KlarnaSignInManager.mm
@@ -9,14 +9,16 @@
 #import "KlarnaSignInManager.h"
 #import <RNKlarnaMobileSDK/RNKlarnaMobileSDK.h>
 #import "KlarnaMobileSDK/KlarnaMobileSDK-Swift.h"
+#import <AuthenticationServices/AuthenticationServices.h>
+#import <UIKit/UIKit.h>
 
-@interface KlarnaSignInManager() <KlarnaEventHandler>
+@interface KlarnaSignInManager() <KlarnaEventHandler, ASWebAuthenticationPresentationContextProviding>
 @property (strong, nonatomic) KlarnaSignInSDK *signInSDK;
 @end
 
 @implementation KlarnaSignInManager
 
-RCT_EXPORT_MODULE()
+RCT_EXPORT_MODULE(RNKlarnaSignIn)
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params { 
     return std::make_shared<facebook::react::NativeKlarnaSignInSpecJSI>(params);
@@ -28,10 +30,11 @@ RCT_EXPORT_MODULE()
         locale:(NSString *)locale
 tokenizationId:(NSString *)tokenizationId {
     _signInSDK = [[KlarnaSignInSDK alloc]
-                  initWithEnvironment: KlarnaEnvironment.production
+                  initWithEnvironment: KlarnaEnvironment.staging
                   region: KlarnaRegion.eu
-                  returnUrl: [NSURL URLWithString:@""]
+                  returnUrl: [NSURL URLWithString:@"in-app-test://siwk"]
                   eventHandler: self];
+    [_signInSDK signInClientId:clientId scope:scope market:market locale:locale tokenizationId:tokenizationId presentationContext:self];
     NSLog(@"Sign should trigger with the following values:\n clientId: %@\nscope: %@\nmarket: %@\nlocale: %@\ntokenizationId: %@\n", clientId, scope, market, locale, tokenizationId);
 }
 
@@ -43,6 +46,27 @@ tokenizationId:(NSString *)tokenizationId {
 
 - (void)klarnaComponent:(id <KlarnaComponent> _Nonnull)klarnaComponent encounteredError:(KlarnaError * _Nonnull)error {
     
+}
+
+// MARK: - ASWebAuthenticationPresentationContextProviding
+
+- (ASPresentationAnchor)presentationAnchorForWebAuthenticationSession:(ASWebAuthenticationSession *)session {
+    UIWindowScene *windowScene = (UIWindowScene *)[[[UIApplication sharedApplication].connectedScenes objectsPassingTest:^BOOL(UIScene * _Nonnull obj, BOOL * _Nonnull stop) {
+        if(obj.activationState == UISceneActivationStateForegroundActive) {
+            *stop = YES; // Stop after finding the first match
+            return YES;
+        }
+        return NO;
+    }] allObjects].firstObject;
+
+    if (windowScene != nil) {
+        UIWindow *currentTopWindow = windowScene.windows.firstObject;
+        if (currentTopWindow != nil) {
+            return currentTopWindow;
+        }
+    }
+    
+    return ASPresentationAnchor();
 }
 
 @end
