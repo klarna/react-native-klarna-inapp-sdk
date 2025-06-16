e412d03cb93f04c44de95fabd02cfe1e170e1bf2
Nuri Mertcan Guner <9140452+NMGuner@users.noreply.github.com>
2025-06-16 18:49:19 +0200

    upgrade android implementation to rn 0.80

--- TestApp/App.tsx
+++ TestApp/App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
